8eb7001c702a5c6e10cba1d5e65d3c18fd7dcc91
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-10-05 16:21:40 -0700

    Ran formatter

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h> //puts
+#include <stdio.h>  //puts
 #include <string.h> //strncmp
 
 struct command_entry {
@@ -8,31 +8,27 @@ struct command_entry {
 
 typedef struct command_entry command_t;
 
-static command_t commands[] = {
-    {NULL, NULL}
-};
+static command_t commands[] = {{NULL, NULL}};
 
 #define INPUT_BUFFER_SIZE 1024
 
-int main(){
+int main() {
 
     puts("Welcome to SeaShell\nPress Ctrl-C or Ctrl-D to exit");
 
     char input[INPUT_BUFFER_SIZE];
 
-    while(fgets(input, INPUT_BUFFER_SIZE, stdin) != NULL){
+    while (fgets(input, INPUT_BUFFER_SIZE, stdin) != NULL) {
 
         command_t * iter = commands;
-        while(iter->name != NULL){
-            if(strncmp(iter->name, input, strlen(iter->name)) == 0){
+        while (iter->name != NULL) {
+            if (strncmp(iter->name, input, strlen(iter->name)) == 0) {
                 iter->cmd(input);
                 break;
             }
         }
 
         // We reach the end of the array without finding a command
-        if(iter->name == NULL){
-            puts("Unrecognized command");
-        }
+        if (iter->name == NULL) { puts("Unrecognized command"); }
     }
 }

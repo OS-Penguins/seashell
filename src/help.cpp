47960086507cdd3d217347de6d39ba9913eac366
jasonling1 <61610371+jasonling1@users.noreply.github.com>
2020-10-19 15:35:14 -0700

    Update src/help.cpp
    Co-authored-by: Nicholas Baron <nicholas.baron.ten@gmail.com>

--- src/help.cpp
+++ src/help.cpp
@@ -9,13 +9,14 @@ namespace seashell {
 
 void help(const std::string &)
 {
-	std::cout<<"Yeehaw here is a list of commands: \n";
-	std::cout<<"ls - displays files in directory\n";
-	std::cout<<"cd <directory> - changes current directory to <directory>\n";
-	std::cout<<"mkdir/rmdir <directory> - creates or deletes <directory>\n";
-	std::cout<<"Telari - :)\n";
+	std::cout << 
+	"Yeehaw here is a list of commands: \n" 
+	"ls - displays files in directory\n"
+	"cd <directory> - changes current directory to <directory>\n"
+	"mkdir/rmdir <directory> - creates or deletes <directory>\n"
+	"Telari - :)" << std::endl;
+
 
 }
 }
-
 

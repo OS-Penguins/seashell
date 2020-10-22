e09a8bdcd6dab171a558329ab6a42b451bfa51a0
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-10-21 17:13:02 -0700

    fixed mkdir issues, allow more arguments

--- src/mkdir.cpp
+++ src/mkdir.cpp
@@ -2,15 +2,28 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <cctype>
 #include <iostream>
 void sub1(const std::string & input);
 
 void seashell::mkdir(const std::string & input) {
-    sub1(input.substr(input.find(" "), input.length()));
+    std::string foldername;
+    for (auto i = input.find("r") + 1; i < input.size(); i++) {
+        auto character = input.at(i);
+        if (isspace(character)) {
+            if (not foldername.empty()) {
+                sub1(foldername);
+                foldername.clear();
+            }
+        } else {
+            foldername += character;
+        }
+    }
+    if (not foldername.empty()) { sub1(foldername); }
 }
 
 // sub-function made to differ the mkdir() functions
 void sub1(const std::string & input) {
-    std::cout << input << std::endl;
-    mkdir(input.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    if (mkdir(input.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
+        perror("mkdir");
 }

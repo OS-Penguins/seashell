b938fd6d47ad9697b54269cfea34564f75b27ba8
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-10-23 15:57:56 -0700

    removed forward declaration and iostream

--- src/mkdir.cpp
+++ src/mkdir.cpp
@@ -3,27 +3,23 @@
 #include <sys/types.h>
 
 #include <cctype>
-#include <iostream>
-void sub1(const std::string & input);
-
-void seashell::mkdir(const std::string & input) {
-    std::string foldername;
-    for (auto i = input.find("r") + 1; i < input.size(); i++) {
-        auto character = input.at(i);
-        if (isspace(character)) {
-            if (not foldername.empty()) {
-                sub1(foldername);
-                foldername.clear();
-            }
-        } else {
-            foldername += character;
-        }
-    }
-    if (not foldername.empty()) { sub1(foldername); }
-}
 
 // sub-function made to differ the mkdir() functions
 void sub1(const std::string & input) {
     if (mkdir(input.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1)
         perror("mkdir");
+}
+
+// command for mkdir
+void seashell::mkdir(const std::string & input) {
+    std::string foldername;
+    for (auto i = input.find("r") + 1; i < input.size(); i++) {
+        if (auto character = input.at(i); not isspace(character)) {
+            foldername += character;
+        } else if (not foldername.empty()) {
+            sub1(foldername);
+            foldername.clear();
+        }
+    }
+    if (not foldername.empty()) { sub1(foldername); }
 }

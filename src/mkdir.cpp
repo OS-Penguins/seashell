d2a93b92055e3fb2804d541e41bb1b2dd055ca05
Howard <36084984+hmontesdeoca@users.noreply.github.com>
2020-10-23 16:31:40 -0700

    Update src/mkdir.cpp single chracter R not string R
    Co-authored-by: Nicholas Baron <nicholas.baron.ten@gmail.com>

--- src/mkdir.cpp
+++ src/mkdir.cpp
@@ -13,7 +13,7 @@ static void sub1(const std::string & input) {
 // command for mkdir
 void seashell::mkdir(const std::string & input) {
     std::string foldername;
-    for (auto i = input.find("r") + 1; i < input.size(); i++) {
+    for (auto i = input.find('r') + 1; i < input.size(); i++) {
         if (auto character = input.at(i); not isspace(character)) {
             foldername += character;
         } else if (not foldername.empty()) {

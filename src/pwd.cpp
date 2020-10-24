40b798c3772dd8643f677c25157e410f59b64220
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-10-23 17:37:26 -0700

    fixed memeber contrib

---
+++ src/pwd.cpp
@@ -0,0 +1,9 @@
+#include "commands.h"
+
+#include <unistd.h>
+
+// pwd command (does not need to free malloc)
+void seashell::pwd(const std::string &) {
+    const auto * dir_name = get_current_dir_name();
+    puts(dir_name);
+}

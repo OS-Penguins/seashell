8b1e4db058ce7fd341c62a303e9601831a4ecd4b
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-10-27 14:05:42 -0700

    format

--- src/cd.cpp
+++ src/cd.cpp
@@ -14,8 +14,7 @@ void seashell::cd(const std::string & input) {
 
     auto end_path = input.find_last_not_of(" \t\n\r\v");
 
-    auto directory = input.substr(start_path, end_path+1 - start_path);
+    auto directory = input.substr(start_path, end_path + 1 - start_path);
 
-    if(chdir(directory.c_str()))
-	    perror("cd");
+    if (chdir(directory.c_str())) perror("cd");
 }

b67d908a82b823e22c35b27f3c908ee205538190
jasonling1 <61610371+jasonling1@users.noreply.github.com>
2020-10-27 13:22:02 -0700

    Add files via upload

---
+++ src/commands.h
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <string>
+
+namespace seashell {
+    void ls(const std::string &);
+    void mkdir(const std::string&);
+    void copy(const std::string &);
+    void rmdir(const std::string&);
+    void cd(const std::string&);
+    void help(const std::string&);
+    void exit(const std::string&);
+    // Exec is special
+    bool exec(const std::string &);
+}
+
+#endif

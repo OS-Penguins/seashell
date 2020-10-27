9544326e73e5611b87130efa2e38f7b0b9e07288
jasonling1 <61610371+jasonling1@users.noreply.github.com>
2020-10-26 18:43:47 -0700

    Add files via upload

---
+++ src/cd.cpp
@@ -0,0 +1,23 @@
+#include "commands.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <cctype>
+#include <iostream>
+#include <unistd.h>
+
+
+void seashell::cd(const std::string & input) {
+    const auto end_of_cd = input.find('d');
+
+    auto start_path = end_of_cd + 1;
+    while (isspace(input[start_path])) start_path++;
+
+    auto end_path = input.find_last_not_of(" \t\n\r\v");
+
+    auto directory = input.substr(start_path, end_path - start_path);
+    
+    chdir(directory);
+
+
+}

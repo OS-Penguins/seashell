6182384da9818121eaeff0cadd5ae5d3e6cd93d3
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-10-27 16:07:38 -0700

    added secret talari command

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ using command = std::function<void(const std::string &)>;
 static const std::map<std::string, command> commands{
     {"ls", seashell::ls},     {"mkdir", seashell::mkdir}, {"cp", seashell::copy},
     {"pwd", seashell::pwd},   {"rmdir", seashell::rmdir}, {"cd", seashell::cd},
-    {"exit", seashell::exit}, {"help", seashell::help},
+    {"exit", seashell::exit}, {"help", seashell::help},   {"talari", seashell::talari},
 };
 
 static std::string first_word(const std::string & input) {
